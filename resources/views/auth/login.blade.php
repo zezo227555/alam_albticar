3f362bb36bfdb7850bb2965960909ab36e04d931
zezo227555 <zidan4773@gmail.com>
2024-11-26 17:55:10 +0200

    last update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>منظومة الدراسة و الامتحانات</title>
+    <title>عالم الابتكار</title>
     @include('includes.head')
 </head>
 
--- resources/views/auth/student_login.blade.php
+++ resources/views/auth/student_login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>بوابة الطالب</title>
+    <title>عالم الابتكار</title>
     @include('includes.head')
 </head>
 
